b7fe1890c380da36ad2a2d23e1d98d97e50295f5
Shane McDonough <shanemcdman@gmail.com>
2022-07-13 16:55:37 -0700

    Document quote.rs

--- src/quote.rs
+++ src/quote.rs
@@ -1,10 +1,13 @@
+//! Used to get quotes from <https://api.quotable.io/random>
 use serde::Deserialize;
 
+/// Holds response from <https://api.quotable.io/random>
 #[derive(Deserialize)]
 struct Response {
     content: String,
 }
 
+/// Use reqwest to get quotes from <https://api.quotable.io/random>
 pub fn random_quote() -> String {
     reqwest::blocking::get("https://api.quotable.io/random")
         .expect("Couldn't get url")
